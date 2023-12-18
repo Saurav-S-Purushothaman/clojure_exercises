844b013c6073b881a3131d7a73237e91ee92557d
Saurav S Purushothaman <saurav.kudajadri@gmail.com>
2023-12-18 22:36:32 +0530

    binary-search working

--- binary-search.clj
+++ binary-search.clj
@@ -1,12 +1,13 @@
-(defn binary-search [arr target] 
-  (let [cnt (- (count arr) 1)]
-   (loop [left 0 right cnt]
-     (if (>= left right) nil 
-       (let [mid (/ (+ left right) 2) mid-val (nth arr mid)]
-         (cond 
-           (= mid-val target) mid-val
-           (> mid-val target) (recur (inc mid) right)
-           :else (recur left (dec mid))))))))
+;; binary search algorithm implemented in clojure.
+(defn binary-search [arr target]
+  (loop [left 0 right (count arr)]
+    (if (>= left right) nil 
+      (let [mid (/ (+ left right) 2)]
+        (cond
+          (= target (nth arr mid)) mid
+          (> target (nth arr mid)) (recur (inc mid) right)
+          :else (recur left (dec mid)))))))
 
-(def vec [1 2 3 4 5 6])
-(def target 4)
+(def vecs [1 2 3 4 5 6])
+(def x (binary-search vecs 4))
+(str "The value of x is " x )
