b49df93a8fffdbe43c1352a18bc256897410ebd9
Saurav S Purushothaman <saurav.kudajadri@gmail.com>
2023-12-18 22:37:33 +0530

    implemented account balance transaction using refs and dosync

--- serpant_talk/src/serpant_talk/talk.clj
+++ serpant_talk/src/serpant_talk/talk.clj
@@ -8,3 +8,29 @@
   (println x "Hello, World!"))
 
 (def hello (csk/->snake_case "hello world"))
+
+(def counter (ref 0))
+
+(let [n 5]
+  (future (dotimes [_ n] (alter counter inc)))
+  (future (dotimes [_ n] (alter counter inc)))
+  (future (dotimes [_ n] (alter counter inc))))
+
+@counter
+
+;; going to transfer 50 from account balance one to account balance 2 synchronously
+;; using multi-thread
+
+;; initial setup
+(def account1 (ref 1000))
+(def account2 (ref 2000))
+
+(defn transfer [amt]
+  (dosync (alter account1 #(- % amt)) (alter account2 #(+ % amt))))
+
+(let [n 3] 
+  (future (dotimes [_ n] (transfer 100)))
+  (future (dotimes [_ n] (transfer 200))))
+
+(str "The value of account 1 is " @account1 " and the value of account 2 is " @account2)
+
