0013f5c36f9311a539546310f2793e834985e0ba
Saurav S Purushothaman <saurav.kudajadri@gmail.com>
2023-12-19 21:07:25 +0530

    changed file for exps

--- binary-search.clj
+++ binary-search.clj
@@ -1,12 +1,14 @@
 ;; binary search algorithm implemented in clojure.
-(defn binary-search [arr target]
-  (loop [left 0 right (count arr)]
-    (if (>= left right) nil 
-      (let [mid (/ (+ left right) 2)]
-        (cond
-          (= target (nth arr mid)) mid
-          (> target (nth arr mid)) (recur (inc mid) right)
-          :else (recur left (dec mid)))))))
+(defn binary-search 
+  "Binary-Search algorithm implemented in clojure"
+  [arr target]
+    (loop [left 0 right (count arr)]
+      (if (>= left right) nil 
+        (let [mid (/ (+ left right) 2)]
+          (cond
+            (= target (nth arr mid)) mid
+            (> target (nth arr mid)) (recur (inc mid) right)
+            :else (recur left (dec mid)))))))
 
 (def vecs [1 2 3 4 5 6])
 (def x (binary-search vecs 4))
