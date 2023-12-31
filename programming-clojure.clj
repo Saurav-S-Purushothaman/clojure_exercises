9d8171e47fa1354a6089e62a6588b41512a4887f
Saurav S Purushothaman <saurav.kudajadri@gmail.com>
2023-12-31 11:37:11 +0530

    programming clojure

--- programming-clojure.clj
+++ programming-clojure.clj
@@ -58,8 +58,65 @@
  ([] (greeting "world")) 
  ([username] (str "Hello " username)))
 
+;; destructuring 
+;; 
+;; (defn greeting
+;;   [{:keys [fname lname]}]
+;;   (println "Hell " fname))
 
+;; the above code doesn't work. 
+;; instead use this.
+(defn greeting
+  [{fname :first-name}]
+  (println "Hello " fname))
 
+(defn greet-author-2 [{fname :first-name}]
+(println "Hello," fname))
 
+(require '[clojure.string :as str])
+
+(defn ellipsize [words]
+(let [[w1 w2 w3] (str/split words #"\s+")]
+(str/join " " [w1 w2 w3 "..."])))
+(ellipsize "The quick brown fox jumps over the lazy dog.")
+
+;; resolve 
+;; it tells the symbol or var it resolves to in the current namespace. 
+;; (resolve sym)
+(resolve 'ellipsize)
+;; namespace. 
+;; best way to switch namespace is by using the command
+;; (in-ns namespace-name)
+(in-ns 'myapp)
+;; when you are in new namespace always use this
+
+(clojure.core/use 'clojure.core)
+
+;; be default class name outside of java.lang must be fully qualified in clojure
+;; for example 
+;;(File/separator) ;; this throws an error.
+;; you must use
+
+(java.io.File/separator)
+
+;; you should use import for importing java classes 
+;; require for importing vars from another namespace.
+;; syntax (import ('package class1 class2 ...))
+
+;; different type of requre statement
+(require 'clojure.string)
+;; even if you import using this statement, you are still required to use 
+;; the fully qualified name of the underlying methods of this namespace. 
+;; one way to avoid that is to use alias
+(require ['clojure.string :as str])
+;; now you may use the alias to use the functions of clojure.string.
+;; it is common practice to set namespace using ns command.
+;; then use the additional :import, :require and :use commands to 
+;; import java classes, and namespaces.
+;; an eg. would be
+
+;; (ns example.exploring
+;;   (:require ['clojure.string :as str])
+;;   (:import (java.io File )))
 
 
--- test.clj
+++ test.clj
@@ -1,12 +1,16 @@
-(println "Hello world")
-(Thread/sleep 1000)
-(println "hello world again")
+(defn binary-search
+  "Finds earliest occurrence of target in arr (a vector) using binary search."
+  ([arr target]
+     (loop [low 0 high (unchecked-dec (count arr))]
+       (if (<= high (inc low))
+         (cond
+           (== target (arr low)) low
+           (== target (arr high)) high
+           :else nil)
+         (let [m (unchecked-add low (bit-shift-right (unchecked-subtract high low) 1))]
+           (if (< (arr m) target)
+             (recur (unchecked-inc m) high)
+             (recur low m)))))))
 
-;; this was an interesting program.
-(defn say-hello-ten-times[]
-  (loop [counter 0]
-    (when (< counter 10)
-      (do 
-        (println "INFO: Try number = " counter)
-        (Thread/sleep 2000)
-        (recur (inc counter))))))
+(println (binary-search [1 2 3 4 5 7 8 9 10 11 12 13 14 15 16 17 19 20 21 22 23 24 25 26 28] 28))
+
